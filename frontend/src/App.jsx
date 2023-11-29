8a889b5cd3783af96b5949cf5ad3c077ce67b8f1
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2023-11-29 19:28:49 +0600

    tailwind initialization
    installed tailwind, working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,48 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  var t1 = "[[1, 2, 3]], [4, 1, 2], [2, 1, 5]]"
-  var t2 = "[[1]], [2], [5]]"
-  var t3 = "[[1, 2, 3]], [4, 1, 2], [2, 1, 5]]"
-
   return (
-    <>
-    <div class="parent">
-        <div class="navbar">
-            <div class="name">
-                <h1>TensorITS</h1>
-            </div>
-            <div class="optionbar">
-                <div class="time">time</div>
-                <div class="quantity">quantity</div>
-                <div class="divider">1</div>
-                <div class="number">1 2 3 4 5</div>
-            </div>
-            <div class="userid">userid</div>
-        </div>
-        <div class="inputbar1">
-            <div class="leftbar">
-              {t1}
-            </div>
-            <div class="rightbar">
-                {t2}
-            </div>
-        </div>
-        <div class="inputbar2">
-            <div class="middlebar">
-                {t3}
-            </div>
-        </div>
-        <div class="coding">
-            <h2>start typing your code...</h2>
-        </div>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
-    </>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () =>{
+
+    return(
+    <div className="w-screenwidth bg-gray-800 h-32">
+        Navbar
+    </div>
+    );
+    
+    };
+    
+    export default Navbar;
