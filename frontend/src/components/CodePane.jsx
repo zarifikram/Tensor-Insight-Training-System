64d77073a1e7b947e1dbd783322cd095a0e46f34
zarifikram <65703522+zarifikram@users.noreply.github.com>
2024-01-25 22:17:42 +0600

    Update CodePane.jsx
    Needs work:
    Caret width and color

--- frontend/src/components/CodePane.jsx
+++ frontend/src/components/CodePane.jsx
@@ -1,15 +1,51 @@
 import { AuthContext } from "./helpers/AuthContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
-const CodePane = () =>{
-    const [authState,setAuthState]= useContext(AuthContext);
-    return(
-    <div>
-        <div className={`pt-20 ${authState.textcolor2} font-roboto text-2xl font-bold`}>0/8</div>
-        <div className={` ${authState.textcolor} font-roboto text-2xl font-bold`}>start typing your code . . .</div>
-    </div>
-    );
-    
+const CodePane = () => {
+    const [authState, setAuthState] = useContext(AuthContext);
+    const [code, setCode] = React.useState("Start typing your code...");
+    const handleChange = (event) => {
+        setCode(event.target.value);
+    }
+
+    const onKeyDown = (e) => {
+        if (e.key === "Tab") {
+            e.preventDefault();
+            document.execCommand('insertText', false, '    ');
+            // setCode(e.target.value+"\t");
+        }
     };
-    
-    export default CodePane;
+
+    const preRef = useRef();
+
+    useEffect(() => {
+        // Set focus on the pre element when the component mounts
+        preRef.current.focus();
+    }, []);
+
+    return (
+        <div>
+            <div className={`pt-20 ${authState.textcolor2} font-roboto text-2xl font-bold`}>0/8</div>
+            {/* <div className={` ${authState.textcolor} font-roboto text-2xl font-bold`}>lol typing your code . . .</div> */}
+            <div className="py-4">
+                <pre
+                    ref={preRef}
+                    style={{
+                        selectionColor: 'green',
+                    }}
+                    className={`focus:outline-none p-4 h-60 p-0 overflow-auto font-roboto ${authState.textcolor} whitespace-pre-wrap text-2xl`}
+                    contentEditable
+                    spellCheck="false"
+                    onInput={handleChange}
+                    onKeyDown={onKeyDown}
+                >
+                    {code}
+                </pre>
+
+            </div>
+        </div>
+    );
+
+};
+
+export default CodePane;
