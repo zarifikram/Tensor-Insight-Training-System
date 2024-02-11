e66cc935ea62b1bf7085872e287a3f1aea2c1044
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2024-02-11 17:40:03 +0600

    navbar patch 1

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,13 +7,13 @@ import { FaBoltLightning } from "react-icons/fa6";
 import { IoMdSettings } from "react-icons/io";
 import { HiDotsHorizontal } from "react-icons/hi";//<HiDotsHorizontal />
 import { FaBolt } from "react-icons/fa";
-
+//const navigate = useNavigate();
 
 import { AuthContext } from "./helpers/AuthContext";
 import { ColorContext } from "./helpers/ColorContext";
 import ProblemSet from "./ProblemSet/ProblemSet";
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 import { useState } from "react";
 
@@ -37,6 +37,9 @@ const defaultSettingOfMode = {
 }
 
 const Navbar = () => {
+    
+    const navigate = useNavigate();
+
     const [mode, setMode] = useState({ mode: "quantity", setting: 4 });
     const [colorState, setColorState] = useContext(ColorContext);
 
@@ -56,8 +59,8 @@ const Navbar = () => {
 
     const [isPopupOpen2, setPopupOpen2] = useState(false);
 
-    const openPopup2 = (page) => {
-        setMode("none");
+    const openPopup2 = () => {
+        //setMode("none");
         setPopupOpen2(true);
 
     };
@@ -67,15 +70,15 @@ const Navbar = () => {
     };
 
 
-
     return (
         <div className="w-screenwidth h-28 flex mx-40 py-10">
             <div className={`w-10% flex items-center font-saira text-3xl font-black ${colorState.captioncolor}`}>TensorITS</div>
             <div className={`w-15% flex justify-evenly`}>
                 <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`} onClick={openPopup2}><FaCode /></div>
-                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`}><AiOutlineGlobal /></div>
-                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`}><FaCircleExclamation /></div>
-                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`}>
+                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`} onClick={()=>navigate('/')}><AiOutlineGlobal /></div>
+                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`} onClick={()=>navigate('/AddProblem')}><FaCircleExclamation /></div>
+                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`} onClick={()=>navigate('/ContestList')}><FaBolt  /></div>
+                <div className={`  flex items-center justify-center py-2 font-saira ${colorState.textcolor} font-bold text-xl`} >
                     {<button onClick={openPopup} className={`hover:text-gray-400`}><IoMdSettings /></button>}
                 </div>
             </div>
