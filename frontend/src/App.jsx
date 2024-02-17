1f7ede845b7aa4116a4e65685c1080b627e4577f
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2024-02-18 00:43:58 +0600

    routeContext fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -113,9 +113,7 @@ function App() {
   });
 
   const [csrfState, setCSRFState] = useState();
-  const [routeContext, setRouteContext] = useState({ "isPractice": false, navItemIndex: 4 });
-
-  
+  const [routeContext, setRouteContext] = useState({ isPractice: false, navItemIndex: 4 });
 
   return (
     <div className="font-roboto" >
