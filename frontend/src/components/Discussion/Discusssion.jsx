712cd04db5fe3f7a8bea0688f04ce3de58487f36
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2024-02-25 16:23:52 +0600

    discussion 3

--- frontend/src/components/Discussion/Discusssion.jsx
+++ frontend/src/components/Discussion/Discusssion.jsx
@@ -13,6 +13,28 @@ import { useRef } from "react";
 import { useParams } from "react-router";
 import { useNavigate } from "react-router";
 
+
+const calculateTimeDifference = (time) => {
+  const currentTime = new Date();
+  const pastTime = new Date(time);
+  const difference = currentTime - pastTime;
+
+  const seconds = Math.floor(difference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+      return `${days} day${days !== 1 ? 's' : ''} ago`;
+  } else if (hours > 0) {
+      return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
+  } else if (minutes > 0) {
+      return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+  } else {
+      return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
+  }
+};
+
 const Discussion = () =>{
     const iniDiscussion={
         "id": 1,
@@ -110,42 +132,82 @@ const Discussion = () =>{
     const [discussion,setDiscussion] = useState(iniDiscussion)
  
 
-    // useEffect(() => {
-    //     axios.get(`http://127.0.0.1:8000/api/discussion-forum/${id}/`)
-    //         .then((response) => {
-    //             console.log(response.data);
-    //             setDiscussion(response.data)
-    //         }).catch((error) => {
-    //             console.error("Error fetching data:", error);
-    //         });
-    // }, []); 
+    useEffect(() => {
 
-    const calculateTimeDifference = (time) => {
-      const currentTime = new Date();
-      const pastTime = new Date(time);
-      const difference = currentTime - pastTime;
-  
-      const seconds = Math.floor(difference / 1000);
-      const minutes = Math.floor(seconds / 60);
-      const hours = Math.floor(minutes / 60);
-      const days = Math.floor(hours / 24);
-  
-      if (days > 0) {
-          return `${days} day${days !== 1 ? 's' : ''} ago`;
-      } else if (hours > 0) {
-          return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-      } else if (minutes > 0) {
-          return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
-      } else {
-          return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
-      }
-  };
+      const interval = setInterval(() => {
+        axios.get(`http://127.0.0.1:8000/api/discussion-forum/${id}/`)
+            .then((response) => {
+                console.log(response.data);
+                setDiscussion(response.data)
+            }).catch((error) => {
+                console.error("Error fetching data:", error);
+            });
 
-  const [answer, setAnswer] = useState('');
+          }, 1000); // Run every second (1000 milliseconds)
+
+          // Clean up the interval when the component unmounts or when the dependency changes
+          return () => clearInterval(interval);
+    }, []); 
+
+
+
+  const [answerbox, setAnswerbox] = useState('');
 
   const givanswer = (event) => {
-    setAnswer(event.target.value);
+    setAnswerbox(event.target.value);
   };
+
+  const submitAnswer = () =>{
+    axios.post(`http://127.0.0.1:8000/api/discussion-forum/${id}/add-answer/`,{answer:answerbox})
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+    });
+  }
+
+        //*Edit Answer------------------------------------
+        const [editbox1, setEditbox1] = useState('New Title');
+        const [editbox2, setEditbox2] = useState('New Description');
+
+        const giveedit1 = (event) => {
+          setEditbox1(event.target.value);
+        };
+
+        const giveedit2 = (event) => {
+          setEditbox2(event.target.value);
+        };
+  
+        const submitEdit = () =>{
+          axios.patch(`http://127.0.0.1:8000/api/discussion-forum/${id}/edit/`,{title:editbox1,description:editbox2})
+          .then((response) => {
+              console.log(response.data);
+              setEditbox1('New Title')
+              setEditbox2('New Description')
+          })
+          .catch((error) => {
+              console.error("Error fetching data:", error);
+          });
+        }
+        //*Toggle Boxes--------------------------
+        //const [replyOn,setReplyOn] =useState(false)
+        const [editOn,setEditOn] =useState(false)
+
+      //*Delete Comment----------------------
+      const DeleteComment = () =>{
+        axios.delete(`http://127.0.0.1:8000/api/discussion-forum/${id}/delete/`)
+        .then((response) => {
+            console.log(response.data);
+            navigate(`/DiscussionList`)
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+
+        
+      }
+  
 
     return(
     <div className={`mx-40 ${colorState.textcolor} font-roboto`}>    
@@ -166,21 +228,41 @@ const Discussion = () =>{
               <div className={`flex items-center text-xl ${colorState.captioncolor}`}>
                     <div className={`mr-3`}> <FaUserCircle /></div> {discussion.user.first_name+" "+discussion.user.last_name}</div>
                 </div>
-
-
           </div>
-
           </div>
         </div>
-        <div className="flex">
+        <div className="flex mb-2">
             <textarea className={`w-full h-16 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
-            value={answer}
+            value={answerbox}
             onChange={givanswer} />
-            <div className={`ml-2 w-40 h-16 rounded-md ${colorState.box1color} ${colorState.textcolor2} text-2xl flex justify-center items-center hover:bg-gray-400`}>
+            <div className={`ml-2 w-40 h-16 rounded-md ${colorState.box1color} ${colorState.textcolor2} text-2xl flex justify-center items-center hover:bg-gray-400`} onClick={submitAnswer}>
               Answer
             </div>
         </div>
-
+        <div className="flex mb-2">
+          <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2} text-2xl flex justify-center items-center hover:bg-gray-400 mr-2`} onClick={()=>{setEditOn(prevState => !prevState);}}>
+                Edit
+            </div>
+            <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2} text-2xl flex justify-center items-center hover:bg-gray-400`} onClick={()=>{DeleteComment();}}>
+                Delete
+            </div>
+        </div>{
+          editOn && (<div> 
+            <div className="flex mb-2">
+                <input className={`w-full h-10 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
+                value={editbox1}
+                onChange={giveedit1} />
+            </div>
+            <div className="flex mb-2">
+                <input className={`w-full h-10 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
+                value={editbox2}
+                onChange={giveedit2} />
+            </div>
+            <div className={` w-40 h-10 rounded-md ${colorState.box1color} text-2xl flex justify-center items-center hover:bg-gray-400`} onClick={submitEdit}>
+                  Edit
+            </div>
+        </div>)
+      }
         <div className={` flex mt-4 mb-5 text-2xl font-bold ${colorState.captioncolor}`}>
           
               {discussion.answers.length > 1 ? 
@@ -191,48 +273,204 @@ const Discussion = () =>{
                 {discussion.answers.length} <div className={`ml-2`}>Answer</div>
               </>)}
         </div>
-        {discussion.answers.map((answer, index) => (
-                <div className={`flex mb-8`}>
-                  <div className={`w-16 `}>
-                    <div className={`w-10 h-10 text-xl flex justify-center items-center  rounded-full ${answer.user_vote==="up"? `${colorState.box2color}`:`${colorState.box1color} hover:bg-gray-400`}`}><AiOutlineCaretUp/></div>
-                    <div className={`w-10 h-10 text-xl flex justify-center items-center  ${answer.user_vote!=null?`${colorState.textcolor2}`:``}`}>{answer.vote}</div>
-                    <div className={`w-10 h-10 text-xl flex justify-center items-center  rounded-full ${answer.user_vote==="down"? `${colorState.box2color}`:`${colorState.box1color} hover:bg-gray-400`}`}><AiOutlineCaretDown/></div>
-                  </div>
-                  <div className={`ml-4`}>
-                    <div>  
-                      <div className={`text-xl`}>{answer.answer}</div>
-                    </div>
-                    <div className={`flex justify-start mt-3 mb-2`}>
-                 
-                      <div>
-                      <div className="flex"><div className="mr-2">answered</div>{calculateTimeDifference(answer.created_at)}</div>
-                      <div className={`flex items-center text-xl ${colorState.captioncolor}`}>
-                      <div className={`mr-3`}> <FaUserCircle /></div> {answer.user.first_name+" "+answer.user.last_name}</div>
-                      </div>
-                 
-                  </div>
-                  <div className={`h-1 w-full rounded-md mb-3 ${colorState.box1color}`}></div>
-                      <div className={`ml-10`}>
-                        {answer.reply.map((reply, index) => (
-                          <div className={`flex justify-start mb-5`}>
-                            <div>
-                              <div className={`flex`}>
-                                <div className={`flex items-center `}>
-                                  <div className={`mr-3 `}> <FaUserCircle /></div> {reply.user.first_name+" "+reply.user.last_name}</div>
-                                <div className="flex"><div className="mr-2 ml-5">replied</div>{calculateTimeDifference(reply.created_at)}</div>
-                              </div>
-                              <div>{reply.reply}</div>
-                            </div>
-                            
-                          </div>
-                        ))}
-                      </div>
-                  </div>
-                </div>
-        ))}
+        {(discussion.answers!=null)&&(discussion.answers.map((answer, index) => (
+                <Answer answer={answer} />
+        )))}
     </div>
     );
     
     };
     
     export default Discussion;
+
+
+
+
+    const Answer = ({ answer }) => { 
+      const [colorState,setColorState]= useContext(ColorContext);
+      const [authState,setAuthState] = useContext(AuthContext);
+
+      const [replybox, setReplybox] = useState('');
+
+      const givanswer = (event) => {
+        setReplybox(event.target.value);
+      };
+
+      const submitReply = (id) =>{
+        axios.post(`http://127.0.0.1:8000/api/answer/${id}/reply/`,{reply:replybox})
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+      }
+
+      //*Edit Answer------------------------------------
+      const [editbox, setEditbox] = useState('');
+
+      const giveedit = (event) => {
+        setEditbox(event.target.value);
+      };
+
+      const submitEdit = (id) =>{
+        axios.patch(`http://127.0.0.1:8000/api/answer/${id}/edit/`,{answer:editbox})
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+      }
+
+
+      //*Toggle Boxes--------------------------
+      const [replyOn,setReplyOn] =useState(false)
+      const [editOn,setEditOn] =useState(false)
+
+      //*Delete Comment----------------------
+      const DeleteComment = () =>{
+      axios.delete(`http://127.0.0.1:8000/api/answer/${answer.id}/delete/`)
+      .then((response) => {
+          console.log(response.data);
+      })
+      .catch((error) => {
+          console.error("Error fetching data:", error);
+      });
+    }
+
+      return (
+
+        <div className={`flex mb-8`}>
+        <div className={`w-16 `}>
+          <div className={`w-10 h-10 text-xl flex justify-center items-center  rounded-full ${answer.user_vote==="up"? `${colorState.box2color}`:`${colorState.box1color} hover:bg-gray-400`}`}><AiOutlineCaretUp/></div>
+          <div className={`w-10 h-10 text-xl flex justify-center items-center  ${answer.user_vote!=null?`${colorState.textcolor2}`:``}`}>{answer.vote}</div>
+          <div className={`w-10 h-10 text-xl flex justify-center items-center  rounded-full ${answer.user_vote==="down"? `${colorState.box2color}`:`${colorState.box1color} hover:bg-gray-400`}`}><AiOutlineCaretDown/></div>
+        </div>
+        <div className={`ml-4 w-full`}>
+          <div>  
+            <div className={`text-xl`}>{answer.answer}</div>
+          </div>
+          <div className={`flex justify-start mt-3 mb-2`}>
+       
+            <div>
+            <div className="flex"><div className="mr-2">answered</div>{calculateTimeDifference(answer.created_at)}</div>
+            <div className={`flex items-center text-xl ${colorState.captioncolor}`}>
+            <div className={`mr-3`}> <FaUserCircle /></div> {answer.user.first_name+" "+answer.user.last_name}</div>
+            </div>
+       
+        </div>
+        <div className={`h-1 w-full rounded-md mb-3 ${colorState.box1color}`}></div>
+
+        <div className="flex mb-2">
+            <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2}  flex justify-center items-center hover:bg-gray-400 mr-2`} onClick={()=>{setReplyOn(prevState => !prevState); setEditOn(false);}}>
+                Reply
+            </div>
+          <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2}  flex justify-center items-center hover:bg-gray-400 mr-2`} onClick={()=>{setEditOn(prevState => !prevState);  setReplyOn(false);}}>
+                Edit
+            </div>
+            <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2}  flex justify-center items-center hover:bg-gray-400`}  onClick={()=>{DeleteComment()}}>
+                Delete
+            </div>
+        </div>
+
+{/* reply ------------ */}{
+            replyOn && (<div className="flex mb-2">
+                <input className={`w-full h-10 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
+                value={replybox}
+                onChange={givanswer} />
+                <div className={`ml-2 w-40 h-10 rounded-md ${colorState.box1color}  text-xl flex justify-center items-center hover:bg-gray-400`} onClick={()=>{submitReply(answer.id)}}>
+                  Reply
+                </div>
+            </div>)
+            }
+{/* edit ------------ */}
+{   editOn && (<div className="flex mb-2">
+            <input className={`w-full h-10 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
+                value={editbox}
+                onChange={giveedit} />
+                <div className={`ml-2 w-40 h-10 rounded-md ${colorState.box1color}  text-xl flex justify-center items-center hover:bg-gray-400`} onClick={()=>{submitEdit(answer.id)}}>
+                  Edit
+                </div>
+            </div>)}
+
+            <div className={`ml-10`}>
+                  {
+                    (answer.reply!=null)&&(answer.reply.map((reply, index) => (
+                      <Reply reply={reply} />
+                  )))
+                  }
+            </div>
+        </div>
+      </div>
+      );
+    };
+
+
+    const Reply = ({ reply }) => { 
+      const [colorState,setColorState]= useContext(ColorContext);
+      const [authState,setAuthState] = useContext(AuthContext);
+
+
+            //*Edit Answer------------------------------------
+            const [editbox, setEditbox] = useState('');
+
+            const giveedit = (event) => {
+              setEditbox(event.target.value);
+            };
+      
+            const submitEdit = (id) =>{
+              axios.patch(`http://127.0.0.1:8000/api/reply/${id}/edit/`,{reply:editbox})
+              .then((response) => {
+                  console.log(response.data);
+              })
+              .catch((error) => {
+                  console.error("Error fetching data:", error);
+              });
+            }
+
+            //*Toggle Boxes--------------------------
+   
+             const [editOn,setEditOn] =useState(false)
+
+             //*Delete Comment----------------------
+             const DeleteComment = () =>{
+              axios.delete(`http://127.0.0.1:8000/api/reply/${reply.id}/delete/`)
+              .then((response) => {
+                  console.log(response.data);
+              })
+              .catch((error) => {
+                  console.error("Error fetching data:", error);
+              });
+            }
+
+      return (
+        <div className={` mb-5`}>
+        <div>
+          <div className={`flex`}>
+            <div className={`flex items-center `}>
+              <div className={`mr-3 `}> <FaUserCircle /></div> {reply.user.first_name+" "+reply.user.last_name}</div>
+            <div className="flex"><div className="mr-2 ml-5">replied</div>{calculateTimeDifference(reply.created_at)}</div>
+          </div>
+          <div>{reply.reply}</div>
+        </div>
+        <div className="flex mb-2">
+          <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2}  flex justify-center items-center hover:bg-gray-400 mr-2`} onClick={()=>{setEditOn(prevState => !prevState);}}>
+                Edit
+            </div>
+            <div className={` w-40 h-10 rounded-md ${colorState.box1color} ${colorState.textcolor2}  flex justify-center items-center hover:bg-gray-400`} onClick={()=>{DeleteComment()}}>
+                Delete
+            </div>
+        </div>{
+            editOn && (<div className="flex mb-2">
+              <input className={`w-full h-10 rounded-lg p-4 ${colorState.box1color}  font-roboto  ${colorState.textcolor}  text-xl `}
+                value={editbox}
+                onChange={giveedit} />
+                <div className={`ml-2 w-40 h-10 rounded-md ${colorState.box1color}  text-xl flex justify-center items-center hover:bg-gray-400`} onClick={()=>{submitEdit(reply.id)}}>
+                  Edit
+                </div>
+            </div>)
+          }
+      </div>
+      );
+    };
