f5510b7d51d12827d213a454444b5d38d5f597bd
Sk . Saifullah Hafiz <121357034+sksaifullahhafiz1099@users.noreply.github.com>
2024-02-26 19:12:39 +0600

    indentation fix

--- frontend/src/components/ProblemSet/ProblemSet.jsx
+++ frontend/src/components/ProblemSet/ProblemSet.jsx
@@ -11,7 +11,7 @@ import Filter from "./Filter";
 import axios from 'axios';
 import { useEffect } from "react";
 
-axios.defaults.withCredentials= true;
+axios.defaults.withCredentials = true;
 
 import { useRef } from "react";
 
@@ -23,15 +23,15 @@ function convertSettingsToString(settings) {
     return str;
 }
 
-const ProblemSet = ({ routeContext, setRouteContext }) =>{
+const ProblemSet = ({ routeContext, setRouteContext }) => {
 
     const navigate = useNavigate();
-    const [colorState,setColorState]= useContext(ColorContext);
-    const [authState,setAuthState] = useContext(AuthContext);
+    const [colorState, setColorState] = useContext(ColorContext);
+    const [authState, setAuthState] = useContext(AuthContext);
 
     //*Filtering-----------------------------------------
-    const [is_user_added,setIs_user_added] = useState("");
-    const iniSettings={
+    const [is_user_added, setIs_user_added] = useState("");
+    const iniSettings = {
         "depth": 2,
         "initiator": {
             "randint": false,
@@ -64,16 +64,16 @@ const ProblemSet = ({ routeContext, setRouteContext }) =>{
 
     const [settings, setSettings] = useState(iniSettings)
 
-    const [difficultyGTE,setDifficultyGTE] = useState("")
-    const [difficultyLTE,setDifficultyLTE] = useState("")
-    const [depthGTE,setDepthGTE] = useState("")
-    const [depthLTE,setDepthLTE] = useState("")
+    const [difficultyGTE, setDifficultyGTE] = useState("")
+    const [difficultyLTE, setDifficultyLTE] = useState("")
+    const [depthGTE, setDepthGTE] = useState("")
+    const [depthLTE, setDepthLTE] = useState("")
 
     //*Problems------------------------------------------
     const [perPage, setPerPage] = useState('10');
-    const [currentPage,setCurrentPage] =useState(1);
-    const [currentProblem,setCurrentProblem] = useState(1);
-    const [problems,setProblems] = useState({
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentProblem, setCurrentProblem] = useState(1);
+    const [problems, setProblems] = useState({
         "count": 27,
         "next": "http://127.0.0.1:8000/api/problem-set/?page=3&page_size=10",
         "previous": "http://127.0.0.1:8000/api/problem-set/?page_size=10",
@@ -116,70 +116,70 @@ const ProblemSet = ({ routeContext, setRouteContext }) =>{
 
     //*Probllem List-------------------------------------------------------------------------------------------
     useEffect(() => {
-        let str=convertSettingsToString(settings)
-        
+        let str = convertSettingsToString(settings)
+
         axios.get(`http://127.0.0.1:8000/api/problem-set/?page_size=${perPage}${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
-        .then((response) => {
-        console.log(response.data);
-        setProblems(response.data)
-        }).catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+            .then((response) => {
+                console.log(response.data);
+                setProblems(response.data)
+            }).catch((error) => {
+                console.error("Error fetching data:", error);
+            });
 
         setCurrentPage(1);
-      }, [perPage,is_user_added,settings,depthGTE,depthLTE,difficultyGTE,difficultyLTE]);
+    }, [perPage, is_user_added, settings, depthGTE, depthLTE, difficultyGTE, difficultyLTE]);
 
 
-    const goToPage=(page)=>{
-        let str=convertSettingsToString(settings)
+    const goToPage = (page) => {
+        let str = convertSettingsToString(settings)
 
         axios.get(`http://127.0.0.1:8000/api/problem-set/?page=${page}&page_size=${perPage}${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
-        .then((response) => {
-        console.log(response.data);
-        setProblems(response.data)
-        }).catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+            .then((response) => {
+                console.log(response.data);
+                setProblems(response.data)
+            }).catch((error) => {
+                console.error("Error fetching data:", error);
+            });
 
         setCurrentPage(page);
     }
 
-    const goForward=()=>{
-        let str=convertSettingsToString(settings)
+    const goForward = () => {
+        let str = convertSettingsToString(settings)
 
-        if(problems.next!=null){
-        axios.get(problems.next+`${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
-        .then((response) => {
-        console.log(response.data);
-        setProblems(response.data)
-        }).catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+        if (problems.next != null) {
+            axios.get(problems.next + `${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
+                .then((response) => {
+                    console.log(response.data);
+                    setProblems(response.data)
+                }).catch((error) => {
+                    console.error("Error fetching data:", error);
+                });
 
-        if((currentPage)<Math.ceil(problems.count / perPage))
-            setCurrentPage(currentPage+1);
+            if ((currentPage) < Math.ceil(problems.count / perPage))
+                setCurrentPage(currentPage + 1);
         }
     }
 
-    const goBackward=()=>{
-        let str=convertSettingsToString(settings)
+    const goBackward = () => {
+        let str = convertSettingsToString(settings)
 
-        if(problems.previous!=null){
-        axios.get(problems.previous+`${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
-        .then((response) => {
-        console.log(response.data);
-        setProblems(response.data)
-        }).catch((error) => {
-            console.error("Error fetching data:", error);
-        });
+        if (problems.previous != null) {
+            axios.get(problems.previous + `${str}&is_user_added=${is_user_added}&difficulty__gte=${difficultyGTE}&difficulty__lte=${difficultyLTE}&depth__gte=${depthGTE}&depth__lte=${depthLTE}`)
+                .then((response) => {
+                    console.log(response.data);
+                    setProblems(response.data)
+                }).catch((error) => {
+                    console.error("Error fetching data:", error);
+                });
 
-        if((currentPage-1)>0)
-            setCurrentPage(currentPage-1);
-    }
+            if ((currentPage - 1) > 0)
+                setCurrentPage(currentPage - 1);
+        }
     }
 
     //*------------------------------------------------------------------------------------------------------------
-    const enterProblem =(id)=>{
+    const enterProblem = (id) => {
         navigate(`/Problem/${id}`);
         setCurrentProblem(id);
     }
@@ -190,17 +190,17 @@ const ProblemSet = ({ routeContext, setRouteContext }) =>{
     };
 
     //*--------------------------------------------------------------------------------------------------------
-    const [isFilterPopUpOpen,setFilterPopUpOpen]=useState(false)
-    const OpenFilter = () =>{
+    const [isFilterPopUpOpen, setFilterPopUpOpen] = useState(false)
+    const OpenFilter = () => {
         setFilterPopUpOpen(true);
         console.log(isFilterPopUpOpen);
     }
 
-    const CloseFilter = () =>{
+    const CloseFilter = () => {
         setFilterPopUpOpen(false);
     }
 
-    const cleanFilter = () =>{
+    const cleanFilter = () => {
         setSettings(iniSettings);
         setIs_user_added("")
         setDifficultyGTE("")
@@ -209,15 +209,15 @@ const ProblemSet = ({ routeContext, setRouteContext }) =>{
         setDepthLTE("")
     }
 
-    return(
+    return (
         <div className={`mx-40 ${colorState.textcolor} font-roboto`}>
-              <div className={` `}>
+            <div className={` `}>
                 <div className={`text-2xl ${colorState.captioncolor} font-bold pb-5`}>Problem Set</div>
                 <div className={`flex justify-end pb-5`}>
-                        <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 mr-3 rounded-md hover:bg-gray-400`} onClick={() => navigate("/AddProblem")}>Add Problem</div>
-                        <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 mr-3 rounded-md hover:bg-gray-400`} onClick={() => {cleanFilter()}}>Remove Filters</div>
-                        <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 rounded-md hover:bg-gray-400`} onClick={() => {OpenFilter()}}>Filters</div>
-                    </div>
+                    <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 mr-3 rounded-md hover:bg-gray-400`} onClick={() => navigate("/AddProblem")}>Add Problem</div>
+                    <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 mr-3 rounded-md hover:bg-gray-400`} onClick={() => { cleanFilter() }}>Remove Filters</div>
+                    <div className={`${colorState.box1color} flex justify-center items-center px-3 py-3 rounded-md hover:bg-gray-400`} onClick={() => { OpenFilter() }}>Filters</div>
+                </div>
                 <div className={`flex w-100% justify-between pb-1`}>
                     <div className={`flex w-50% justify-start ${colorState.textcolor2}`}>
                         <div className={`w-10% flex justify-center`}>id</div>
@@ -230,55 +230,55 @@ const ProblemSet = ({ routeContext, setRouteContext }) =>{
                         <div className={`w-25% flex justify-center`}>difficulty</div>
                     </div>
                 </div>
-                 {Array.from({ length: problems.results.length }, (_, index) => (    
-                <div className={` `} onClick={() => enterProblem(problems.results[index].id)}>
-                    <div  className={`${(index+1)%2===1 ?`${colorState.box1color} w-100% flex justify-between rounded-md hover:bg-gray-400 hover:rounded-md py-2`:`w-100% flex justify-between hover:bg-gray-400 hover:rounded-md py-2`}`}>
-                        <div className={`flex w-50% justify-start`}>
-                            <div className={`w-10% flex justify-center`}>{problems.results[index].id}</div>
-                            <div className={`pl-20`}>{problems.results[index].used_manipulator}</div>
-                        </div>
-                        <div className={`flex w-50% justify-between `}>
-                            <div className={`w-25% flex justify-center `}>{problems.results[index].solve_count}</div>
-                            <div className={`w-25% flex justify-center`}>{problems.results[index].try_count!=0 ? (100*problems.results[index].solve_count/problems.results[index].try_count).toFixed(2)+"%" : "0.00%"}</div>
-                            <div className={`w-25% flex justify-center`}>{problems.results[index].depth}</div>
-                            <div className={`w-25% flex justify-center`}>{problems.results[index].difficulty}</div>
+                {Array.from({ length: problems.results.length }, (_, index) => (
+                    <div className={` `} onClick={() => enterProblem(problems.results[index].id)}>
+                        <div className={`${(index + 1) % 2 === 1 ? `${colorState.box1color} w-100% flex justify-between rounded-md hover:bg-gray-400 hover:rounded-md py-2` : `w-100% flex justify-between hover:bg-gray-400 hover:rounded-md py-2`}`}>
+                            <div className={`flex w-50% justify-start`}>
+                                <div className={`w-10% flex justify-center`}>{problems.results[index].id}</div>
+                                <div className={`pl-20`}>{problems.results[index].used_manipulator}</div>
+                            </div>
+                            <div className={`flex w-50% justify-between `}>
+                                <div className={`w-25% flex justify-center `}>{problems.results[index].solve_count}</div>
+                                <div className={`w-25% flex justify-center`}>{problems.results[index].try_count != 0 ? (100 * problems.results[index].solve_count / problems.results[index].try_count).toFixed(2) + "%" : "0.00%"}</div>
+                                <div className={`w-25% flex justify-center`}>{problems.results[index].depth}</div>
+                                <div className={`w-25% flex justify-center`}>{problems.results[index].difficulty}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
                 ))}
-                    <div className={`flex w-100% justify-between pt-5`}>
-                        <select className={`${colorState.box1color} py-2 rounded-md `}
+                <div className={`flex w-100% justify-between pt-5`}>
+                    <select className={`${colorState.box1color} py-2 rounded-md `}
                         onChange={handleSelectChange}
                         value={perPage}
-                        >
-                            <option value="10">10/page</option>
-                            <option value="20">20/page</option>
-                            <option value="30">30/page</option>
-                            <option value="50">50/page</option>
-                        </select>
-                        <div className={`flex justify-end`}>
-                            <div className={`${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400 flex items-center`} onClick={goBackward}><IoIosArrowBack/></div>
-                            {Array.from({ length: Math.ceil(problems.count / perPage) }, (_, index) => (  
-                            <div className={`${(index+1)==currentPage?`${colorState.box2color} py-2 px-4 ml-2 rounded-md hover:bg-grey-400 `:`${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400`}`} 
-                            onClick={() =>goToPage(index+1)}>{index+1}</div>
-                            ))}
-                            <div className={`${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400 flex items-center `} onClick={goForward}><IoIosArrowForward/></div>
-                        </div>   
+                    >
+                        <option value="10">10/page</option>
+                        <option value="20">20/page</option>
+                        <option value="30">30/page</option>
+                        <option value="50">50/page</option>
+                    </select>
+                    <div className={`flex justify-end`}>
+                        <div className={`${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400 flex items-center`} onClick={goBackward}><IoIosArrowBack /></div>
+                        {Array.from({ length: Math.ceil(problems.count / perPage) }, (_, index) => (
+                            <div className={`${(index + 1) == currentPage ? `${colorState.box2color} py-2 px-4 ml-2 rounded-md hover:bg-grey-400 ` : `${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400`}`}
+                                onClick={() => goToPage(index + 1)}>{index + 1}</div>
+                        ))}
+                        <div className={`${colorState.box1color} py-2 px-4 ml-2 rounded-md hover:bg-gray-400 flex items-center `} onClick={goForward}><IoIosArrowForward /></div>
                     </div>
                 </div>
-                {
-                    <Filter isOpen={isFilterPopUpOpen}  onClose={CloseFilter} 
-                     is_user_added={is_user_added} setIs_user_added={setIs_user_added} 
-                     settings={settings} setSettings={setSettings} 
-                     difficultyGTE={difficultyGTE} setDifficultyGTE={setDifficultyGTE}
-                     difficultyLTE={difficultyLTE} setDifficultyLTE={setDifficultyLTE}
-                     depthGTE={depthGTE} setDepthGTE={setDepthGTE}
-                     depthLTE={depthLTE} setDepthLTE={setDepthLTE}
-                     ></Filter>
-                }
-      </div>
+            </div>
+            {
+                <Filter isOpen={isFilterPopUpOpen} onClose={CloseFilter}
+                    is_user_added={is_user_added} setIs_user_added={setIs_user_added}
+                    settings={settings} setSettings={setSettings}
+                    difficultyGTE={difficultyGTE} setDifficultyGTE={setDifficultyGTE}
+                    difficultyLTE={difficultyLTE} setDifficultyLTE={setDifficultyLTE}
+                    depthGTE={depthGTE} setDepthGTE={setDepthGTE}
+                    depthLTE={depthLTE} setDepthLTE={setDepthLTE}
+                ></Filter>
+            }
+        </div>
     );
-    
-    };
-    
-    export default ProblemSet;
+
+};
+
+export default ProblemSet;
